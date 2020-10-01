4f77db9928e066aa3aa923c29bf879bf198332c6
Kronykus <MoreheadSteve@gmail.com>
2020-09-30 22:27:08 -0500

    day 1+

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,18 +2,41 @@
 
 import sys
 
+
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [ 0 ] * 256
+        self.reg = [ 0 ] * 8
+        self.pc  =   0
+
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        address  = 0
+        filename = sys.argv[ 1 ]
 
+        with open( filename ) as f:
+            for line in f:
+                line = line.split( '#' )
+                line = line[ 0 ].strip()
+
+                if line == '':
+                    continue
+
+                self.ram[ address ] = int( line, 2 )
+                address += 1
+
+    """ 
         # For now, we've just hardcoded a program:
 
         program = [
@@ -25,10 +48,20 @@ class CPU:
             0b00000000,
             0b00000001, # HLT
         ]
-
+ 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram[ address ] = instruction
             address += 1
+    """
+
+    # Memory Address Register (MAR)
+    # Memory Data    Register (MDR)
+    def ram_read( self, MAR ):
+        return self.ram[ MAR ]
+
+
+    def ram_write( self, MAR, MDR ):
+        self.ram[ MAR ] = MDR
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +95,22 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        
+        while self.running:
+            # Instruction Register (IR)
+            IR = self.ram_read( self.pc )
+
+            op_a = self.ram_read( self.pc + 1 )
+            op_b = self.ram_read( self.pc + 2 )
+
+            if IR == HLT:
+                self.running = False
+            elif IR == LDI:
+                self.reg[ op_a ] = [ op_b ]
+                self.pc += 3
+            elif IR == PRN:
+                print( self.reg[ op_a ] )
+                self.pc += 2
+            else:
+                print( f'Instruction Register Unknown: {IR} at program counter {self.pc}' )
+                self.running = False
